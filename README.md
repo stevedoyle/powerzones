302c4b107b9fd5c776bd68015e118015b8c7952e
Stephen Doyle <stephendoyle75@gmail.com>
2022-11-05 17:15:21 +0000

    Initial implementation

---
+++ README.md
@@ -0,0 +1,25 @@
+# powerzones
+
+A utility library for calculating cycling power zones using [Hunter Allen's formula](https://www.hunterallenpowerblog.com/2015/05/power-training-zones-101.html).
+
+
+## Usage
+
+Add this to your `Cargo.toml`:
+
+```toml
+[dependencies]
+powerzones = "0.1.0"
+```
+
+Example usage:
+
+```rust
+let ftp: u32 = 200;
+let zones = calc_power_zones(ftp);
+```
+
+## License
+
+Rand is distributed under the terms of both the MIT license and the
+Apache License (Version 2.0).
---
+++ src/lib.rs
@@ -0,0 +1,55 @@
+use serde::Serialize;
+
+/// A structure representing a power zone.
+///
+/// Each power zone is represented by its name, and a power range represented
+/// by a lower and upper power in Watts.
+#[derive(Serialize, Debug, PartialEq)]
+pub struct Zone {
+    pub name: String,
+    /// Lower end of the power zone
+    pub lower: u32,
+    /// Upper end of the power zone
+    pub upper: u32,
+}
+
+/// Calculate power zones based on an Functional Threshold Power (FTP) value.
+///
+/// # Example
+///
+/// ```
+/// let ftp: u32 = 200;
+/// let zones = calc_power_zones(ftp);
+/// ```
+pub fn calc_power_zones(ftp: u32) -> Vec<Zone> {
+    let mut zones: Vec<Zone> = Vec::new();
+    let mut lower = 0;
+    for (name, upper_percent) in vec![
+        ("Recovery", 0.55), ("Endurance", 0.75), ("Tempo", 0.9),
+        ("Threshold", 1.05), ("VO2Max", 1.2), ("Anaerobic Capacity", 1.5)] {
+        let upper = watts(ftp, upper_percent);
+        zones.push(Zone{name: name.to_string(), lower, upper});
+        lower = upper+1;
+    }
+    zones
+}
+
+fn watts(ftp: u32, target: f32) -> u32 {
+    ((ftp as f32) * target).round() as u32
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_zone1() {
+        let zones = calc_power_zones(189);
+        assert_eq!(zones[0], Zone{name: "Recovery".to_owned(), lower: 0, upper: 104});
+        assert_eq!(zones[1], Zone{name: "Endurance".to_owned(), lower: 105, upper: 142});
+        assert_eq!(zones[2], Zone{name: "Tempo".to_owned(), lower: 143, upper: 170});
+        assert_eq!(zones[3], Zone{name: "Threshold".to_owned(), lower: 171, upper: 198});
+        assert_eq!(zones[4], Zone{name: "VO2Max".to_owned(), lower: 199, upper: 227});
+        assert_eq!(zones[5], Zone{name: "Anaerobic Capacity".to_owned(), lower: 228, upper: 284});
+    }
+}
